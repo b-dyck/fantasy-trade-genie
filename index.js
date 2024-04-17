0e5d4b7275f51f557c9cb8ca3630e6e56108abf4
Bryce Dyck <bryce@Bryces-MacBook-Pro.local>
2024-04-17 16:45:26 -0700

    create step 2 for oauth flow. Currently attempting to implement step 3, facing bugs

--- index.js
+++ index.js
@@ -1,17 +1,70 @@
 require('dotenv').config();
+const axios = require('axios');
 const express = require('express');
 const app = express();
 const port = 8888;
+const querystring = require('querystring');
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
-app.get('/', (req, res) => {
-    res.send('Fantasy Trade Companion!');
-});
+
+//callback not using callback due to ngrok restictions
+app.get('/callback', (req, res) => {
+    const code = req.query.code || null;
+  
+    axios({
+      method: 'post',
+      url: 'https://api.login.yahoo.com/oauth2/get_token',
+      data: querystring.stringify({
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: REDIRECT_URI
+      }),
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+      },
+    })
+      .then(response => {
+        if (response.status === 200) {
+          res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+        } else {
+          res.send(response);
+        }
+      })
+      .catch(error => {
+        res.send(error);
+      });
+  });
+
+const generateRandomString = length => {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+  
+const stateKey = 'yahoo_auth_state';
+
+app.get('/login', (req, res) => {
+    const state = generateRandomString(16);
+    res.cookie(stateKey, state);
+    const queryParams = querystring.stringify({
+      client_id: CLIENT_ID,
+      response_type: 'code',
+      redirect_uri: REDIRECT_URI,
+      state: state,
+    });
+  
+    res.redirect(`https://api.login.yahoo.com/oauth2/request_auth?${queryParams}`);
+  });
+
 
 app.listen(port, () => {
-    console.log(`Express app listening at http://localhost:${port}`);
+    console.log(`Express app listening at https://localhost:${port}`);
 });
 
