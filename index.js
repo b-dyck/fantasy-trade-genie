de58668254387d2823dff77c8a4893d4403e327a
Bryce Dyck <bryce@Bryces-MacBook-Pro.local>
2024-04-17 08:37:18 -0700

    Initial setup using npm nodemon and dotenv. Create index.js starting page, and prepare app with the client id and secret from yahoo

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 8888;
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+app.get('/', (req, res) => {
+    res.send('Fantasy Trade Companion!');
+});
+
+app.listen(port, () => {
+    console.log(`Express app listening at http://localhost:${port}`);
+});
+
